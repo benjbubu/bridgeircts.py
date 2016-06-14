def6bcb4bae5b826a457edae3951f3359c06b3f6
benjbubu <benjbubu@barakuun.com>
2016-06-14 20:38:39 +0200

    Rajout et modification definition de base

--- bridgeircts.py
+++ bridgeircts.py
@@ -4,12 +4,13 @@ import ts3
 
 from sopel.module import commands
 #Debut du tslist
-@module.commands('tslist')
-
+@commands('tslist')
+#Definition des login/mdp du serverquery
 def tslist(bot, trigger):
 	with ts3.query.TS3Connection("localhost") as ts3conn:
         ts3conn.login(
                 client_login_name="",
                 client_login_password=""
         )
-
+#Selection du serveur virtuel
+ts3conn.use(sid=1)
