1fb5dd5dbf2d48ae49247e52ccadc592c1f00e6a
benjbubu <benjbubu@barakuun.com>
2016-06-17 10:48:54 +0200

    Trigger initial fonctionnel.
    Fonctionnalite a rajouter :
    
    -> Enlever le serverquery du resultat de tslist.
    -> rajouter les channels et trier les pseudos

--- bridgeircts.py
+++ bridgeircts.py
@@ -1,24 +1,23 @@
 import sopel.module
 import ts3
 
-
-with ts3.query.TS3Connection("localhost") as ts3conn:
-	try:
-		ts3conn.login(
-			client_login_name="gipsy",
-			client_login_password="XXXXX"
-		)
-	except ts3.query.TS3QueryError as err:
-		print("Login failes:", err.resp.error["msg"])
-		exit(1)
-
-	print("Login OK")
-	ts3conn.use(sid=1)
-	print("Selection server OK")
-
 from sopel.module import commands
 @commands('tslist')
-def tslist(ts3conn, bot, trigger):
-	resp=ts3conn.clientlist()
-	for client in resp.parsed:
-		bot.msg(client)
+def tslist(bot,trigger):
+	with ts3.query.TS3Connection("localhost") as ts3conn:
+		try:
+			ts3conn.login(
+				client_login_name="gipsy",
+				client_login_password="XXXXX"
+			)
+		except ts3.query.TS3QueryError as err:
+			print("Login failed:", err.resp.error["msg"])
+			exit(1)
+
+		print("Login OK")
+		ts3conn.use(sid=1)
+		print("Selection server OK")
+		resp=ts3conn.clientlist()
+		for client in resp.parsed:
+			bot.say(client["client_nickname"])
+
